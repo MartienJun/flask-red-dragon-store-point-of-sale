cb18bb846de6b0b0e1fe5f5eeb6a22ece1e02d15
MartienJun <martienjunaedi123@gmail.com>
2021-04-02 10:12:11 +0700

    Update db.py documentation

--- myapp/db.py
+++ myapp/db.py
@@ -1,22 +1,22 @@
 import sqlite3
 
 import click
-from flask import current_app, g
+from flask import current_app, g #note -current_app-, -g-
 from flask.cli import with_appcontext
 
 
 def get_db():
     if 'db' not in g:
-        g.db = sqlite3.connect(
+        g.db = sqlite3.connect( #note -sqlite3.connect-
             current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES
         )
-        g.db.row_factory = sqlite3.Row
+        g.db.row_factory = sqlite3.Row #note -sqlite3.Row-
 
     return g.db
 
 
-def close_db(e=None):
+def close_db(e=None): #note -close_db-
     db = g.pop('db', None)
 
     if db is not None:
@@ -26,30 +26,30 @@ def close_db(e=None):
 """
 Note:
 
-- current_app -
+-current_app-
 current_app is another special object that points to the Flask application handling the request. 
 Since you used an application factory, there is no application object when writing the rest of 
 your code. get_db will be called when the application has been created and is handling a request, 
 so current_app can be used.
 
 
-- g -
+-g-
 g is a special object that is unique for each request. It is used to store data that might be 
 accessed by multiple functions during the request. The connection is stored and reused instead 
 of creating a new connection if get_db is called a second time in the same request.
 
 
-- sqlite3.connect() -
+-sqlite3.connect()-
 sqlite3.connect() establishes a connection to the file pointed at by the DATABASE configuration key. 
 This file doesn’t have to exist yet, and won’t until you initialize the database later.
 
 
-- sqlite3.Row -
+-sqlite3.Row-
 sqlite3.Row tells the connection to return rows that behave like dicts. This allows accessing the 
 columns by name.
 
 
-- close_db -
+-close_db-
 close_db checks if a connection was created by checking if g.db was set. If the connection exists, 
 it is closed. Further down you will tell your application about the close_db function in the 
 application factory so that it is called after each request.
