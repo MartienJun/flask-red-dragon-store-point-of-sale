bd1c4ae6d9c899f1707ef4f091ea3b86a6267a03
MartienJun <martienjunaedi123@gmail.com>
2021-04-02 10:09:06 +0700

    Add documentation in db.py

--- myapp/db.py
+++ myapp/db.py
@@ -23,3 +23,34 @@ def close_db(e=None):
         db.close()
 
 
+"""
+Note:
+
+- current_app -
+current_app is another special object that points to the Flask application handling the request. 
+Since you used an application factory, there is no application object when writing the rest of 
+your code. get_db will be called when the application has been created and is handling a request, 
+so current_app can be used.
+
+
+- g -
+g is a special object that is unique for each request. It is used to store data that might be 
+accessed by multiple functions during the request. The connection is stored and reused instead 
+of creating a new connection if get_db is called a second time in the same request.
+
+
+- sqlite3.connect() -
+sqlite3.connect() establishes a connection to the file pointed at by the DATABASE configuration key. 
+This file doesn’t have to exist yet, and won’t until you initialize the database later.
+
+
+- sqlite3.Row -
+sqlite3.Row tells the connection to return rows that behave like dicts. This allows accessing the 
+columns by name.
+
+
+- close_db -
+close_db checks if a connection was created by checking if g.db was set. If the connection exists, 
+it is closed. Further down you will tell your application about the close_db function in the 
+application factory so that it is called after each request.
+"""
