dfdf7c391f34d21f9ee0a58abf5290adeaa5b1d8
MartienJun <martienjunaedi123@gmail.com>
2021-04-02 12:20:59 +0700

    Create schema.sql

---
+++ myapp/schema.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS barang;
+DROP TABLE IF EXISTS pembelian;
+
+CREATE TABLE user (
+    email TEXT PRIMARY KEY ASC NOT NULL,
+    pass TEXT NOT NULL,
+    login_status BOOLEAN NOT NULL,
+    nama TEXT NOT NULL,
+    role STRING (10) NOT NULL
+);
+
+CREATE TABLE barang (
+    id_barang INTEGER PRIMARY KEY ASC AUTOINCREMENT NOT NULL,
+    nama_barang TEXT NOT NULL,
+    tipe_barang TEXT NOT NULL,
+    harga INTEGER NOT NULL,
+    stock INTEGER NOT NULL
+);
+
+CREATE TABLE pembelian (
+    id_pembelian INTEGER PRIMARY KEY ASC AUTOINCREMENT NOT NULL,
+    tgl_pembelian DATETIME NOT NULL,
+    id_produk REFERENCES barang (id_barang) ON DELETE NO ACTION ON UPDATE NO ACTION NOT NULL,
+    bayar INTEGER NOT NULL,
+    id_karyawan REFERENCES user (email) ON DELETE NO ACTION ON UPDATE NO ACTION NOT NULL
+);
