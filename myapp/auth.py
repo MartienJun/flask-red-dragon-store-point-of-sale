43257b1df494c31b6825bf745c6a11035aaa91a3
MartienJun <martienjunaedi123@gmail.com>
2021-04-02 15:27:40 +0700

    Add function to store session

--- myapp/auth.py
+++ myapp/auth.py
@@ -15,6 +15,20 @@ from myapp.db import get_db
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+
 @bp.route('/register', methods=('GET', 'POST')) #note -@bp.route-
 def register():
     if request.method == 'POST':
