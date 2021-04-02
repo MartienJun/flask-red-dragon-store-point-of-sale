7c9054c514c06bc098bd3c64a57ee8ab5ed84d7b
MartienJun <martienjunaedi123@gmail.com>
2021-04-02 15:28:08 +0700

    Add login required

--- myapp/auth.py
+++ myapp/auth.py
@@ -16,6 +16,16 @@ from myapp.db import get_db
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
 
 @bp.before_app_request
 def load_logged_in_user():
