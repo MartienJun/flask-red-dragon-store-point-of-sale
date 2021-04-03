060930e89ed1ccda80567ea2c58f6574651a990b
MartienJun <martienjunaedi123@gmail.com>
2021-04-04 01:54:53 +0700

    Create add_product route

--- myapp/product.py
+++ myapp/product.py
@@ -23,3 +23,40 @@ def list_product():
     return render_template('product/list_product.html', products=products)
 
 
+@bp.route('/add_product', methods=('GET', 'POST'))
+@login_required
+def add_product():
+    if request.method == 'POST':
+        nama_barang = request.form['product_name']
+        tipe_barang = request.form['product_type']
+        harga = request.form['price']
+        stock = request.form['stock']
+        db = get_db()
+        error = None
+
+        if not nama_barang:
+            error = 'Product name is required.'
+        elif not tipe_barang:
+            error = 'Product type is required.'
+        elif not harga:
+            error = 'Product price is required.'
+        elif not stock:
+            error = 'Product stock is required.'
+        elif db.execute( #note -db.execute-
+                    'SELECT id_barang FROM barang WHERE nama_barang = ?', (nama_barang,)
+                ).fetchone() is not None: #note -fetchone()-
+                    error = 'Product name {} is already registered.'.format(nama_barang)
+        
+        if error is not None:
+            flash(error)
+        else:
+            db.execute(
+                'INSERT INTO barang (nama_barang, tipe_barang, harga, stock) VALUES (?, ?, ?, ?)',
+                (nama_barang, tipe_barang, harga, stock)
+            )
+            db.commit()
+            return redirect(url_for('product.list_product'))
+    
+    return render_template('product/add_product.html')
+
+
