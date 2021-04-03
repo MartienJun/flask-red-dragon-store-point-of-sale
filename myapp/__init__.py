9762e766c9a5e3cb4b2517bef8c294f48efa4ed4
MartienJun <martienjunaedi123@gmail.com>
2021-04-03 20:31:25 +0700

    Create purchase blueprint

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -36,4 +36,9 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    from . import purchase
+    app.register_blueprint(purchase.bp)
+    app.add_url_rule('/', endpoint='index')
+    
+    
     return app
