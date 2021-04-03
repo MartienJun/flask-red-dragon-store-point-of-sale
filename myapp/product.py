a48774f0b35f33174525fe16c1bf90855dbfc5df
MartienJun <martienjunaedi123@gmail.com>
2021-04-04 01:54:07 +0700

    Create list_product route

--- myapp/product.py
+++ myapp/product.py
@@ -13,3 +13,13 @@ from myapp.db import get_db
 bp = Blueprint('product', __name__)
 
 
+@bp.route('/list_product')
+def list_product():
+    db = get_db()
+    products = db.execute(
+        'SELECT * FROM barang'
+        ' ORDER BY id_barang ASC'
+    ).fetchall()
+    return render_template('product/list_product.html', products=products)
+
+
