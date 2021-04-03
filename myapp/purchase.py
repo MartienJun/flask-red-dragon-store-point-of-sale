2c2333bafaa59502f1940706ca493cd0f3ed18d7
MartienJun <martienjunaedi123@gmail.com>
2021-04-03 20:32:30 +0700

    Create index route

--- myapp/purchase.py
+++ myapp/purchase.py
@@ -11,3 +11,14 @@ from myapp.auth import login_required
 from myapp.db import get_db
 
 bp = Blueprint('purchase', __name__)
+
+
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT id_pembelian, tgl_pembelian, barang.nama_barang, barang.harga, bayar, user.nama'
+        ' FROM pembelian, barang, user WHERE barang.id_barang = pembelian.id_produk AND user.id = pembelian.id_karyawan'
+        ' ORDER BY id_pembelian DESC'
+    ).fetchall()
+    return render_template('purchase/index.html', posts=posts)
