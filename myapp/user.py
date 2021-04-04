af1aab6a74d00269ea6e557ecb5fb0be065f00b1
MartienJun <martienjunaedi123@gmail.com>
2021-04-04 14:31:09 +0700

    Create function to add user

--- myapp/user.py
+++ myapp/user.py
@@ -32,3 +32,40 @@ def get_user(id, check_author=True):
     return users
 
 
+@bp.route('/add_user', methods=('GET', 'POST'))
+@login_required
+def add_user():
+    if request.method == 'POST':
+        nama = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        role = request.form['role']
+        db = get_db()
+        error = None
+
+        if not nama:
+            error = 'Name is required.'
+        elif not email:
+            error = 'Email is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif not role:
+            error = 'Role is required.'
+        elif db.execute( #note -db.execute-
+            'SELECT id FROM user WHERE email = ?', (email,)
+        ).fetchone() is not None: #note -fetchone()-
+            error = 'Email {} is already registered.'.format(email)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (email, password, login_status, nama, role) VALUES (?, ?, ?, ?, ?)',
+                (email, generate_password_hash(password), False, nama, role)
+            )
+            db.commit()
+            return redirect(url_for('user.list_user'))
+
+        flash(error)
+
+    return render_template('user/add_user.html')
+
+
