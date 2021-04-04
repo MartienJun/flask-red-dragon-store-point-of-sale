d816bf0bcb90b5108d5d6b471f8f5853f9069bb6
MartienJun <martienjunaedi123@gmail.com>
2021-04-04 17:43:56 +0700

    Create add purchase function

--- myapp/purchase.py
+++ myapp/purchase.py
@@ -8,17 +8,58 @@ from flask import url_for
 from werkzeug.exceptions import abort
 
 from myapp.auth import login_required
+from myapp.product import *
 from myapp.db import get_db
 
 bp = Blueprint('purchase', __name__)
 
 
 @bp.route('/')
+@login_required
 def index():
     db = get_db()
-    posts = db.execute(
+    purchases = db.execute(
         'SELECT id_pembelian, tgl_pembelian, barang.nama_barang, barang.harga, bayar, user.nama'
         ' FROM pembelian, barang, user WHERE barang.id_barang = pembelian.id_produk AND user.id = pembelian.id_karyawan'
         ' ORDER BY id_pembelian DESC'
     ).fetchall()
-    return render_template('purchase/index.html', posts=posts)
+    return render_template('purchase/index.html', purchases=purchases)
+
+
+def get_purchase(id, check_author=True):
+    purchases = get_db().execute(
+        'SELECT * FROM pembelian'
+        ' WHERE id_pembelian = ?',(id,)
+    ).fetchone()
+
+    return purchases
+
+
+@bp.route('/add_purchase', methods=('GET', 'POST'))
+@login_required
+def add_purchase():
+    if request.method == 'POST':
+        id_produk = request.form['id_produk']
+        bayar = request.form['bayar']
+        id_karyawan = request.form['id_karyawan']
+        db = get_db()
+        error = None
+
+        if not id_produk:
+            error = 'Product is required.'
+        elif not bayar:
+            error = 'Pay is required.'
+        
+        if error is not None:
+            flash(error)
+        else:
+            db.execute(
+                'INSERT INTO pembelian (id_produk, bayar, id_karyawan) VALUES (?, ?, ?)',
+                (id_produk, bayar, id_karyawan)
+            )
+            db.commit()
+            return redirect(url_for('purchase.index'))
+    
+    return render_template('purchase/add_purchase.html', products=getAllProduct())
+
+
