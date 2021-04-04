c1c5e5c336eef080e0f6bb4e0e97601ceb959613
MartienJun <martienjunaedi123@gmail.com>
2021-04-04 14:29:49 +0700

    Create user blueprint

---
+++ myapp/user.py
@@ -0,0 +1,15 @@
+from flask import Blueprint
+from flask import flash
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
+from werkzeug.exceptions import abort
+
+from myapp.auth import login_required
+from myapp.db import get_db
+
+bp = Blueprint('user', __name__)
+
+
